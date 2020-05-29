c2686a3dd40c5870de6965c55474852689899d6e
root <root@localhost.localdomain>
2020-05-29 15:15:56 +0545

    ALL WORDS IN WORLD

--- Shiritori/bplayer.py
+++ Shiritori/bplayer.py
@@ -1,18 +1,17 @@
-#BOT PlAYING SHITORI, PROVIDE HIM A BIG DICTIONARY AND SEE HOW MUCH CAN HE GUESS BEFORE RUNNING OUT OF WORDS
 import random
 from time import *
 import shitori
 import sys
 import os
-filepath = "/usr/share/dirb/wordlists/common.txt"
+filepath = "all_words_in_world"
 
 if len(sys.argv) == 2:
     filepath = sys.argv[1]
 
 if os.path.exists(filepath):
-    wordlist_old = [line.rstrip('\n') for line in open(filepath)] #oneliner yeah
-    wordlist_new = [word for word in wordlist_old if len(word) >= 3 and not '_' in word and not '-' in word and not '.' in word and not '~' in word and not '1' in word and not '2' in word and not '3' in word and not '4' in word and not '5' in word and not '6' in word and not '7' in word and not '8' in word and not '9' in word and not '0' in word] #oneliner MORE YEAH
-#
+    wordlist_old = [line.rstrip('\n') for line in open(filepath)] 
+    wordlist_new = [word for word in wordlist_old if len(word) >= 3 and not ' ' in word] 
+
 def bot_player(wordlist: list) -> None: 
     status = True
     count = 0
@@ -27,7 +26,7 @@ def bot_player(wordlist: list) -> None:
         os.system("clear") 
         if temp != "GAME OVER!!!":
             print(f"The wordlist is {temp}")
-            sleep(0.2)
+            sleep(0)
             wordlist.remove(rnd)
             count += 1
         else:
