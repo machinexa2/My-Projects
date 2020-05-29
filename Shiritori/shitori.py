d64fa6187b927e9258137792137fc4c155361767
root <root@localhost.localdomain>
2020-05-29 14:48:58 +0545

    YES

--- Shiritori/shitori.py
+++ Shiritori/shitori.py
@@ -3,17 +3,6 @@ import random
 import sys
 from time import *
 
-#COMMENT OUT FROM HERE, ONLY NECESSARY FOR BOT PLAYING
-filepath = "/usr/share/dirb/wordlists/common.txt"
-
-if len(sys.argv) == 2:
-    filepath = sys.argv[1]
-
-if os.path.exists(filepath):
-    wordlist_old = [line.rstrip('\n') for line in open(filepath)] #oneliner yeah
-    wordlist_new = [word for word in wordlist_old if len(word) >= 3 and not '_' in word and not '-' in word and not '.' in word and not '~' in word and not '1' in word and not '2' in word and not '3' in word and not '4' in word and not '5' in word and not '6' in word and not '7' in word and not '8' in word and not '9' in word and not '0' in word] #oneliner MORE YEAH
-#COMMENT SHOULD BE ENDED HERE
-
 class SHITori:
 
     def __init__(self):
